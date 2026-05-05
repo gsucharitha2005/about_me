64e4b6a001b433ae0d6abb6fc9b6d6121e76e1a2
g.sucharitha2005 <gsucharitha2005@gmail.com>
2026-05-05 21:36:11 +0530

    Delete about_me.py

--- about_me.py
+++
@@ -1,7 +0,0 @@
-print("about me")
-print("name:sucharitha")
-print("age:21")
-print("college:svcet")
-print("course:3rd year CSE-DS")
-print{"hobbies"reading books,learning new languages")
-print("goal:To become a software engineer")
